0832a4131e6e21f9f5d99ebbfa92df41d43d1e8e
HUGO ALEXANDRE COSTA FERREIRA SOARES <HSOARES1@stu.chi.ac.uk>
2020-02-26 11:06:27 +0000

    increasing jpg size for images from twitter

--- classes/User.php
+++ classes/User.php
@@ -344,7 +344,7 @@ class User
                 foreach ($media as $m) {
                     $mediaHTML = $mediaHTML . '
                     <div class="carousel-item ' . $status . '">
-                        <img class="img-fluid mx-auto d-block rounded " src="' . $m . '?name=small" alt="Article Image">
+                        <img class="img-fluid mx-auto d-block rounded " src="' . $m . '?name=medium" alt="Article Image">
                     </div>';
                     $status = '';
                     // $mediaHTML = $mediaHTML . '<img alt="Article Image" src="' . $m . '?name=small" class="media-img">';
