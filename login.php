40a7542aaf9ad408a3097792cad6739e941da643
HUGO ALEXANDRE COSTA FERREIRA SOARES <HSOARES1@stu.chi.ac.uk>
2020-02-27 11:49:13 +0000

    fixing login functionality

--- login.php
+++ login.php
@@ -3,8 +3,8 @@ define('CONFIG_PROTECTION', false);
 $title = 'UoC Community News';
 $pageId = 'login';
 require_once __DIR__ .'/config.php';
-if (!isset($_SESSION['username'])) {
-    signMeIn($CFG->authmethod);
+if (!isset($_SESSION['signedIn'])) {
+    require_once $CFG->dirroot . '/inc/php/authenticate.php';;
     header('Location: timeline.php');
 }
 die();
