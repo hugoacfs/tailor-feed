6bf3369e9665b8ef3161d4a3468bde1f45f6634e
HUGO ALEXANDRE COSTA FERREIRA SOARES <HSOARES1@stu.chi.ac.uk>
2020-02-27 16:00:01 +0000

    changing function of authenticate

--- inc/php/authenticate.php
+++ inc/php/authenticate.php
@@ -4,18 +4,11 @@ if (!defined('CONFIG_PROTECTION')) {
     http_response_code(403);
     exit;
 }
-$isLoggedIn = $_SESSION['signedIn'] ?? false;
-if (!$isLoggedIn) {
-    if ($CFG->authmethod === 'SSAML') {
-        $_SESSION['USER'] = new SSAML();
-    } else {
-        $loginArray = Authenticate::requestUserInput();
-        $_SESSION['USER'] = new Authenticate($loginArray['username'],$loginArray['password'],$loginArray['givenname']);
-    }
-    $_SESSION['signedIn'] = $_SESSION['USER']->getSignedIn() ?? false;
-    $_SESSION['givenName'] = $_SESSION['USER']->getGivenName() ?? null;
-    $_SESSION['role'] = $_SESSION['USER']->getRole() ?? null;
-    $_SESSION['userId'] = $_SESSION['USER']->getUserId() ?? null;
-    $_SESSION['userName'] = $_SESSION['USER']->getUserName() ?? null;
-    $_SESSION['currentUser'] = new User($_SESSION['userName']);
+if (!$_SESSION['signedIn']) {
+    session_unset();
+    redirectGuestToLogin();
+} elseif (isset($_SESSION['userName'])) {
+    $CURRENTUSER = new User($_SESSION['userName']);
+} else {
+    redirectGuestToLogin();
 }
