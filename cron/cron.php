919b8dd3a68c8406ff29596a30143e04dde86b5c
HUGO ALEXANDRE COSTA FERREIRA SOARES <HSOARES1@stu.chi.ac.uk>
2020-02-28 16:02:37 +0000

    adding feature update to Cron

--- cron/cron.php
+++ cron/cron.php
@@ -1,32 +1,21 @@
 <?php
-if (php_sapi_name() != 'cli') {
-    header('HTTP/1.0 403 Forbidden', true, 403);
-    http_response_code(403);
-    exit;
-}
+// if (php_sapi_name() != 'cli') {
+//     header('HTTP/1.0 403 Forbidden', true, 403);
+//     http_response_code(403);
+//     exit;
+// }
 define('CONFIG_PROTECTION', false);
 require __DIR__ .'/../config.php';
-echo "Starting ".__DIR__."/cron.php \n";
+echo "Getting ".__DIR__."/cron.php ...\n";
 $cronStartTime = time();
 echo "Server Time: ".date( 'r',$cronStartTime)."\n";
-echo "Instanciating Twitter objs... \n";
-$sources = Twitter::getAllSources('twitter');
-//Tweet publisher
-foreach ($sources as $source) {
-    echo('Building articles for '.$source->getReference()." object... \n");
-    $source->buildArticles();
-    echo('Publishing articles for '.$source->getReference(). " object... \n");
-    Article::publishArticles($source->getArticles());
-}
-// Set to true to update the Name and Image path of all Sources in DB
-$updateSourcesDetails = false;
-if($updateSourcesDetails){
-    $cron2ndStartTime = time();
-    echo('Updating all sources details... '."\n");
-    Source::updateAllSourcesDetails();
-    echo('Done! '."\n");
-}
+
+$twitter = new Cron('twitter');
+// $facebook = new Cron('facebook');
+// $rss = new Cron('rss');
+
+
 $cronEndTime = time();
-echo "Cron job finished at: ".date( 'r',$cronEndTime)."\n";
+echo "All cron jobs finished at: ".date( 'r',$cronEndTime)."\n";
 $jobDuration = $cronEndTime - $cronStartTime;
 echo "Duration = ". $jobDuration ." seconds.\n";
