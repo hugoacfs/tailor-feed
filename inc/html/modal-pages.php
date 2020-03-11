de4f5e7fe4c5697ef50eda2f432f0d8ee47f91f5
Hugo Soares <hsoares1@stu.chi.ac.uk>
2020-03-11 09:13:20 +0000

    user feedback updates - enhancing ux

--- inc/html/modal-pages.php
+++ inc/html/modal-pages.php
@@ -10,7 +10,7 @@ if (!defined('CONFIG_PROTECTION')) {
     <div class="modal-dialog modal-dialog-scrollable" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="pagesModalCenteredLabel">Pages</h5>
+                <h5 class="modal-title" id="pagesModalCenteredLabel">Following</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
