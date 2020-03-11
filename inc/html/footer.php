30d5fa8a99218c7da2fbbac4489e84b4c3a0c3f6
HUGO ALEXANDRE COSTA FERREIRA SOARES <HSOARES1@stu.chi.ac.uk>
2020-03-11 10:30:35 +0000

    fixing dependencies, ditching bs bundle for popper and bootstrap

--- inc/html/footer.php
+++ inc/html/footer.php
@@ -5,15 +5,25 @@
         paths: {
             jquery: ['https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min'],
             fontawesome: ['https://kit.fontawesome.com/2a953cdc29'],
-            bootstrap: 'dependencies/bootstrap/bootstrap.bundle.min',
+            bootstrap: ['https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min', 'dependencies/bootstrap/bootstrap.min'],
+            popper: ['https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min', 'dependencies/popper.min'],
             toggle: '../vendor/bootstrap4-toggle-master/js/bootstrap4-toggle',
             lodash: '../vendor/lodash/lodash',
             sortable: '../vendor/sortable/js/sortable.min',
             methods: 'methods'
         },
         shim: {
+            'bootstrap': {
+                'deps': ['jquery']
+            },
             "toggle": ["jquery"],
             "sortable": ["bootstrap"]
+        },
+        map: {
+            '*': {
+                'popper.js': 'popper'
+            }
         }
     });
 </script>
+<!-- https://stackoverflow.com/questions/46004087/issue-loading-popperjs-and-bootstrap-via-requirejs-even-after-using-recommended -->
