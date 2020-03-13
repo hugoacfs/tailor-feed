9ccc6530a12dca67c0d9268e35d06bee131cf536
HUGO ALEXANDRE COSTA FERREIRA SOARES <HSOARES1@stu.chi.ac.uk>
2020-03-13 13:15:31 +0000

    fixes to fetching html for api

--- api/fetch-html.php
+++ api/fetch-html.php
@@ -2,49 +2,10 @@
 define('CONFIG_PROTECTION', false);
 session_start();
 require_once __DIR__ . '/../config.php';
-/**
- *  An example CORS-compliant method.  It will allow any GET, POST, or OPTIONS requests from any
- *  origin.
- *
- *  In a production environment, you probably want to be more restrictive, but this gives you
- *  the general idea of what is involved.  For the nitty-gritty low-down, read:
- *
- *  - https://developer.mozilla.org/en/HTTP_access_control
- *  - http://www.w3.org/TR/cors/
- *
- */
-function cors()
-{
-    global $CFG;
-    // Allow from any origin
-    if (isset($_SERVER['HTTP_ORIGIN'])) {
-        // Decide if the origin in $_SERVER['HTTP_ORIGIN'] is one
-        if (in_array($_SERVER['HTTP_ORIGIN'], $CFG->authorised_cors_array)) exit('Your site is not whitelisted!');
-        // you want to allow, and if so:
-        header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
-        header('Access-Control-Allow-Credentials: true');
-        header('Access-Control-Max-Age: 86400');    // cache for 1 day
-    }
-
-    // Access-Control headers are received during OPTIONS requests
-    if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-
-        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
-            // may also be using PUT, PATCH, HEAD etc
-            header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-
-        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
-            header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
-
-        exit(0);
-    }
-
-    //echo "You have CORS!";
-}
 cors();
 $username = $_SESSION['userName'] ?? 'default';
 //Server url
-$url = $CFG->api_url . "?user=$username&page=1&mode=html";
+$url = $CFG->api_url . "/json.php?user=$username&page=1&mode=html";
 $apiKey = $CFG->json_secret; // should match with Server key
 $headers = array(
     'api_key: ' . $apiKey
