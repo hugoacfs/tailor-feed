021fd26043ed5d0e1ed98a49f6baf5b0c17e0e40
HUGO ALEXANDRE COSTA FERREIRA SOARES <HSOARES1@stu.chi.ac.uk>
2020-03-17 13:33:06 +0000

    typo

--- classes/User.php
+++ classes/User.php
@@ -260,7 +260,7 @@ class User
          * will be displayed
          */
         if (isset($builder['lastArticle'])) {
-            if ($this->userName != 'default') return '';
+            if ($this->userName == 'default') return '';
             $sourcesButton =  '<button type="button" class="pages-btn btn btn-dark btn-outline-light mr-1 ml-1 border" data-toggle="modal" data-content="pages" data-target="#pagesModal">
                             <span class="fas fa-at menu-fa" aria-hidden="true"></span> 
                             <span class="preferences-btn-text">Following</span>
