2d68f69515070accbe7dd9838e37f6c756d96b18
Hugo Soares <hsoares1@stu.chi.ac.uk>
2020-03-18 14:36:42 +0000

    fix to modal close

--- inc/html/modal-images.php
+++ inc/html/modal-images.php
@@ -10,20 +10,9 @@
 <div class="modal fade" id="carousel-modal" tabindex="-1" role="dialog" aria-labelledby="Image preview" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content mx-auto text-center">
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body p-0 ">
+            <div class="modal-body p-0 " data-dismiss="modal" aria-label="Close">
                 <div id="carousel-lander">
-
                 </div>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
             </div>
         </div>
     </div>
