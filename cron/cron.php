2bbfb46dc07fc650289044b33d74a36e85716b7b
Hugo Soares <hsoares1@stu.chi.ac.uk>
2020-03-18 09:52:09 +0000

    minor changes to cron.php to prevent browser access

--- cron/cron.php
+++ cron/cron.php
@@ -1,9 +1,9 @@
 <?php
-// if (php_sapi_name() != 'cli') {
-//     header('HTTP/1.0 403 Forbidden', true, 403);
-//     http_response_code(403);
-//     exit;
-// }
+if (php_sapi_name() != 'cli') {
+    header('HTTP/1.0 403 Forbidden', true, 403);
+    http_response_code(403);
+    exit;
+}
 define('CONFIG_PROTECTION', false);
 require __DIR__ .'/../config.php';
 echo "Getting ".__DIR__."/cron.php ...\n";
@@ -11,7 +11,7 @@ $cronStartTime = time();
 echo "Server Time: ".date( 'r',$cronStartTime)."\n";
 
 $twitter = new Cron('twitter');
-
+// disabled until supported
 // $facebook = new Cron('facebook');
 // $rss = new Cron('rss');
 
